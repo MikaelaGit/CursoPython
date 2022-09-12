ee727f5c43d24859ac61f735cce6523517e391c7
Martinez Micaela <108555206+MikaelaGit@users.noreply.github.com>
2022-09-12 08:42:41 -0300

    Creado con Colaboratory

---
+++ CP 34 moduloa.py
@@ -0,0 +1,79 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyORrMb9lZtMSwhVp07Efrv+",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/MikaelaGit/CursoPython/blob/main/CP%2034%20moduloa.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "4CPOmarBYXkf"
+      },
+      "outputs": [],
+      "source": [
+        "#Curso python 34 módulos "
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "que son los módulos \n",
+        "\n",
+        "Un archivo con extensión .py .pyc (python compilado) o un archivo escrito en C para CPython, que posee su propio espacio de nombres y que puede contener variables, funciones, clases e incluso otros módulos.\n",
+        "\n",
+        "Para que sirven \n",
+        "\n",
+        "para organizar y reutilizar el código (modularización y reutilización)\n",
+        "\n",
+        "como se crea un módulo \n",
+        "\n",
+        "tan sencillo como crear un archivo con extensión .py (o pyc o archivo C) y guardarlo donde nos interese."
+      ],
+      "metadata": {
+        "id": "v5vAiiTnc_vK"
+      }
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "def sumar(op1, op2):\n",
+        "    print('El resultado de la suma es:  ', op1 + op2)\n",
+        "\n",
+        "def restar(op1, op2):\n",
+        "    print('El resultado de la suma es:  ', op1 - op2)\n",
+        "\n",
+        "def multiplicar(op1, op2):\n",
+        "    print('El resultado de la suma es:  ', op1*op2)\n",
+        "\n"
+      ],
+      "metadata": {
+        "id": "VsRgKclsfrJQ"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
